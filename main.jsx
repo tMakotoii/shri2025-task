f38edbe533e6b6e7338fd8b068c6ef9f938c5dca
DoppelGinger <mr.tachibana@mail.ru>
2025-07-05 15:04:13 +0500

    styles

--- main.jsx
+++ main.jsx
@@ -212,7 +212,9 @@ const TABS = {
     ],
   },
 };
-
+for (let i = 0; i < 6; ++i) {
+  TABS.all.items = [...TABS.all.items, ...TABS.all.items];
+}
 const TABS_KEYS = Object.keys(TABS);
 
 function Main() {
